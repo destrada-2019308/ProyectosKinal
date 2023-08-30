482826a6df6cbcb4e0d4bd7d239c4cd9c6b6a11f
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2023-08-30 05:40:56 -0600

    Version 2

--- Juego ahorcado/js/script.js
+++ Juego ahorcado/js/script.js
@@ -2,6 +2,8 @@
 const listaPalabras = ["guitarra",
 "teclado","reloj","tarjeta","tapadera","vaso","pachon","filete","luz","trapo","ventana","puerta","sol","pelota","edificio"]
 const palabra = document.querySelector(".palabra-ul");
+let palabraActual;
+const botonesDelTeclado = document.querySelector(".teclado");
 
 
 //Creamos una funcion flecha para que nos de una palabra aleatoria
@@ -13,8 +15,11 @@ const palabraRandom = () =>{
     // el .length es para contar String, pero también sirve para contar los arreglos
     //que existan en el
     const lista = listaPalabras[Math.floor(Math.random() * listaPalabras.length)]
+    palabraActual = lista
     //Probamos que funcione las palabras aleartorias
-    console.log(lista);}
+    console.log(lista);
+    palabra.innerHTML = lista.split("").map(() => `<li class="palabra"></li>`).join("");
+}
 
 /*
 Funciones flecha
@@ -23,6 +28,27 @@ const nombreDeLaFuncion = (parametro, parametro2) =>{
     document.write(frase + "<br>");
 }
 */
+
+const presion = (button, palabraPresionada) => {
+    if (palabraActual.includes(palabraPresionada)) {
+        [...palabraActual].forEach((palabras, index) =>{
+            if(palabras === palabraPresionada){
+                palabra.querySelectorAll("li")[index].innerText = palabras;
+                palabra.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })        
+    }else {
+
+    }
+}
+
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+        
+    button.innerHTML = String.fromCharCode(i);
+    botonesDelTeclado.appendChild(button);
+    button.addEventListener("click", e => presion(e.target,String.fromCharCode(i)));
+}
 
 palabraRandom();
 
